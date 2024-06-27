541ada6a5d9786ddef16ca25126eeaeb24997730
Saif Ali <93928356+Saif-ali-shekh@users.noreply.github.com>
2024-06-27 17:10:58 +0530

    adding rds

--- booklist/settings.py
+++ booklist/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'booklist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'saif_mysql_database',
+        'USER': 'admin',
+        'PASSWORD': 'Saif22122000',
+        'HOST': 'database-3.cdim6y2005d5.ap-south-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
